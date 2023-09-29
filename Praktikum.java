a14f81d4e04c5bd4ed2430618200a1332ab90569
Pavel Bayok <pashka310@yandex.by>
2023-09-29 13:09:40 +0300

    первая версия программы день программиста

---
+++ Praktikum.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            // здесь нужно вывести результат
+            System.out.println("12.09." + year + " Високосный год");
+        }else {
+            System.out.println("13.09." + year + " Невисокосный год");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        // здесь нужно определить, является ли переданный год високосным
+        if ((year % 4 == 0) & (year % 100 != 0) | (year % 400 == 0)){
+            return true;
+        }
+        return false;
+    }
+}
